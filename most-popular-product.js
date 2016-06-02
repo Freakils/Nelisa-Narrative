6affaedc3b9200d59e8162ae01c6893e458b1a59
Freakils <luvo@projectcodex.co>
2016-06-03 00:09:40 +0200

    changes

--- most-popular-product.js
+++ most-popular-product.js
@@ -1,7 +1,14 @@
-var fs = require("fs");
+// module.exports = function(){
+  var fs = require("fs");
 
-var list = fs.readFileSync("./data-files/week1.csv", "utf8");
-var myDataToArray = list.split("\n").splice(1).filter(Boolean);
+  var list = fs.readFileSync("./data-files/week1.csv", "utf8");
+    var newList = list.split("\n").splice(1).filter(Boolean);
 
-// var newData = [];
-console.log(myDataToArray);
+    
+
+  });
+
+  return newData;
+  console.log(newData);
+
+// }
--- test/process-the-data_test.js
+++ test/process-the-data_test.js
@@ -1,1 +1,27 @@
 var assert = require("assert");
+
+var products = require("../products");
+
+describe("products", function() {
+
+  it("it should return the most popular products in week1", function() {
+    var weekList = products.csv("./files/week1.csv")
+    assert.equal(products.mostPopular(weekList), "Bananas - loose")
+  })
+
+  it("it should return the most popular products in week2", function() {
+    var weekList = products.csv("./files/week2.csv")
+    assert.equal(products.mostPopular(weekList), "Mixed Sweets 5s")
+  })
+
+  it("it should return the most popular products in week3", function() {
+    var weekList = products.csv("./files/week3.csv")
+    assert.equal(products.mostPopular(weekList), "Milk 1l")
+  })
+
+  it("it should return the most popular products in week4", function() {
+    var weekList = products.csv("./files/week4.csv")
+    assert.equal(products.mostPopular(weekList), "Coke 500ml")
+  })
+
+});
