80c78ff5e619aa99fb7efc50639a59d28d9df402
Freakils <luvo@projectcodex.co>
2016-06-07 22:12:53 +0200

    changes

--- test/process-the-data_test.js
+++ test/process-the-data_test.js
@@ -1,27 +1,40 @@
 var assert = require("assert");
+var products = require("../most-pop-products");
 
-var products = require("../products");
 
-describe("products", function() {
+describe("most popular product each week", function() {
 
-  it("it should return the most popular products in week1", function() {
-    var weekList = products.csv("./files/week1.csv")
+  it("should return the most popular products in week1", function() {
+    var weekList = products.csv("./data-files/week1.csv")
     assert.equal(products.mostPopular(weekList), "Bananas - loose")
   })
 
-  it("it should return the most popular products in week2", function() {
-    var weekList = products.csv("./files/week2.csv")
+  it("should return the most popular products in week2", function() {
+    var weekList = products.csv("./data-files/week2.csv")
     assert.equal(products.mostPopular(weekList), "Mixed Sweets 5s")
   })
 
-  it("it should return the most popular products in week3", function() {
-    var weekList = products.csv("./files/week3.csv")
+  it("should return the most popular products in week3", function() {
+    var weekList = products.csv("./data-files/week3.csv")
     assert.equal(products.mostPopular(weekList), "Milk 1l")
   })
 
-  it("it should return the most popular products in week4", function() {
-    var weekList = products.csv("./files/week4.csv")
+  it("should return the most popular products in week4", function() {
+    var weekList = products.csv("./data-files/week4.csv")
     assert.equal(products.mostPopular(weekList), "Coke 500ml")
   })
 
 });
+
+// describe("least popular product each week", function() {
+//
+//   it("should return the least popular products in week1", function() {
+//     var weekList = products.csv("./data-files/week1.csv")
+//     assert.equal()
+//   })
+//
+//   it("should return the least popular products in week2", function() {
+//     var weekList = products.csv("./data-files/week2.csv")
+//     assert.equal()
+//   })
+// });
